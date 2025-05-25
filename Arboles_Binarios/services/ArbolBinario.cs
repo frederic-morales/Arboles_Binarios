9c27d6dca5c118d01f22f5ea8bceb9f46ad0e505
Frederic Morales <tocomorales30@gmail.com>
2025-05-25 12:12:00 -0600

    Agregando eliminar, buscar a los arboles y permitiendo que el usuario eliga que arboles mostrar

--- Arboles_Binarios/services/ArbolBinario.cs
+++ Arboles_Binarios/services/ArbolBinario.cs
@@ -4,16 +4,24 @@ namespace Arboles_Binarios.services
 {
     public class ArbolBinario
     {
-        public Guid Id { get; private set; } = Guid.NewGuid();
+        private static int contador = 1;
+        public int Id { get; private set; }
         public NodoArbol Raiz { get; private set; }
 
+
+        public ArbolBinario() // Constructor
+        {
+            Id = contador++;
+        }
+
+
+        //Insertar Nodo al Arbol
         public void Insertar(InformacionNodo informacion)
         {
             Raiz = InsertarRecursivo(Raiz, informacion);
         }
         private NodoArbol InsertarRecursivo(NodoArbol nodo, InformacionNodo informacion)
         {
-            //Si llegamos a un nodo null, significa que encontramos un lugar vacío, y allí insertamos el nuevo nodo.
             if (nodo == null)
                 return new NodoArbol(informacion);
 
@@ -25,5 +33,54 @@ namespace Arboles_Binarios.services
             return nodo;
         }
 
+
+        //Eliminar Nodo del Arbol
+        public void Eliminar(int edad)
+        {
+            Raiz = EliminarRecursivo(Raiz, edad);
+        }
+        private NodoArbol EliminarRecursivo(NodoArbol nodo, int edad)
+        {
+            if (nodo == null) return null;
+            
+            if (edad < nodo.Informacion.Edad)
+                nodo.Izquierda = EliminarRecursivo(nodo.Izquierda, edad);
+            else if (edad > nodo.Informacion.Edad)
+                nodo.Derecha = EliminarRecursivo(nodo.Derecha, edad);
+
+            else
+            {
+                 if (nodo.Izquierda == null) return nodo.Derecha;
+                 if (nodo.Derecha == null) return nodo.Izquierda;
+
+                var sucesor = EncontrarMinimo(nodo.Derecha);
+                nodo.Informacion = sucesor.Informacion;
+                nodo.Derecha = EliminarRecursivo(nodo.Derecha, sucesor.Informacion.Edad);   
+            }
+
+            return nodo;
+        }
+        private NodoArbol EncontrarMinimo(NodoArbol nodo)
+        {
+            while (nodo.Izquierda != null)
+                nodo = nodo.Izquierda;
+            return nodo;
+        }
+
+
+        //Buscar Nodo en el Arbol
+        public NodoArbol Buscar(int edad)
+        {
+            return BuscarRecursivo(Raiz, edad);
+        }
+        private NodoArbol BuscarRecursivo(NodoArbol nodo, int edad)
+        {
+            if (nodo == null || nodo.Informacion.Edad == edad)
+                return nodo;
+            if (edad < nodo.Informacion.Edad)
+                return BuscarRecursivo(nodo.Izquierda, edad);
+            else
+                return BuscarRecursivo(nodo.Derecha, edad);
+        }
     }
 }
